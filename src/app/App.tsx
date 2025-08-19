138ba05e7e2c31c90f5bf03f6e6979c78c2e9170
Vladimir Taraban <tarabanvv@mail.ru>
2025-08-19 17:12:47 +0800

    роутинг

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,14 @@
-import { Link, Route, Routes } from "react-router";
-import { MainPageAsync } from "../pages/MainPage/ui/MainPage.async";
-import { AboutPageAsync } from "../pages/AboutPage/ui/AboutPage.async";
-import { Suspense } from "react";
+import { Link } from "react-router";
+
 import { useTheme } from "./providers/ThemeProbiders/useTheme";
 import { classNames } from "../shared/lib/classNames/classNames";
+import { AppRouter } from "./providers/router";
 
 export default function App() {
   const { theme, toggleTheme } = useTheme();
   return (
     <div className={classNames("app", {}, [theme])}>
-      <Suspense fallback={<div>"Loading..."</div>}>
-        <Routes>
-          <Route path="/" element={<MainPageAsync />} />
-          <Route path="/about" element={<AboutPageAsync />} />
-        </Routes>
-      </Suspense>
-
+      <AppRouter />
       <Link to={"/"}>MainPage</Link>
       <Link to={"/about"}>AboutPage</Link>
 
