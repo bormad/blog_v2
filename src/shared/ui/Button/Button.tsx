0620241624a6b629a368a3914dfbb3643ee8f527
Vladimir Taraban <tarabanvv@mail.ru>
2025-08-21 22:48:40 +0800

    сраный сторибук так и не заработал не понимаю что ему нужно

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -5,6 +5,7 @@ import styles from "./Button.module.scss";
 // eslint-disable-next-line react-refresh/only-export-components
 export enum ThemeButton {
   CLEAR = "clear",
+  OUTLINE = "outline",
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
